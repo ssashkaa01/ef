614374a8ce2ab35d8d8741265895ebff5b5da5d0
STORYM <ssashkaa01@gmail.com>
2020-03-01 00:20:27 +0200

    add hw

---
+++ 29.02.2020/WindowsFormsUniversity/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace WindowsFormsUniversity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.tabControlStudents = new System.Windows.Forms.TabControl();
+            this.tabPageStudents = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridViewSubjects = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.specializationIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.specializationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountOfHourDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.specializationIdDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.subjectBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridViewSpecializations = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.tabControlStudents.SuspendLayout();
+            this.tabPageStudents.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSubjects)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.specializationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSpecializations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.AllowUserToOrderColumns = true;
+            this.dataGridViewStudents.AutoGenerateColumns = false;
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.specializationIdDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.surnameDataGridViewTextBoxColumn,
+            this.phoneDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dataGridViewStudents.DataSource = this.studentBindingSource;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(6, 6);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.RowTemplate.Height = 28;
+            this.dataGridViewStudents.Size = new System.Drawing.Size(755, 334);
+            this.dataGridViewStudents.TabIndex = 0;
+            this.dataGridViewStudents.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewStudents_CellContentClick);
+            this.dataGridViewStudents.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridViewStudents_DataError);
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // tabControlStudents
+            // 
+            this.tabControlStudents.Controls.Add(this.tabPageStudents);
+            this.tabControlStudents.Controls.Add(this.tabPage2);
+            this.tabControlStudents.Controls.Add(this.tabPage1);
+            this.tabControlStudents.Location = new System.Drawing.Point(13, 13);
+            this.tabControlStudents.Name = "tabControlStudents";
+            this.tabControlStudents.SelectedIndex = 0;
+            this.tabControlStudents.Size = new System.Drawing.Size(775, 379);
+            this.tabControlStudents.TabIndex = 1;
+            // 
+            // tabPageStudents
+            // 
+            this.tabPageStudents.Controls.Add(this.dataGridViewStudents);
+            this.tabPageStudents.Location = new System.Drawing.Point(4, 29);
+            this.tabPageStudents.Name = "tabPageStudents";
+            this.tabPageStudents.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStudents.Size = new System.Drawing.Size(767, 346);
+            this.tabPageStudents.TabIndex = 0;
+            this.tabPageStudents.Text = "Students";
+            this.tabPageStudents.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewSubjects);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(767, 346);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Subjects";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridViewSpecializations);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 346);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Specializations";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(17, 399);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(104, 39);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // dataGridViewSubjects
+            // 
+            this.dataGridViewSubjects.AutoGenerateColumns = false;
+            this.dataGridViewSubjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSubjects.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn1,
+            this.nameDataGridViewTextBoxColumn1,
+            this.amountOfHourDataGridViewTextBoxColumn,
+            this.specializationIdDataGridViewTextBoxColumn1});
+            this.dataGridViewSubjects.DataSource = this.subjectBindingSource;
+            this.dataGridViewSubjects.Location = new System.Drawing.Point(7, 7);
+            this.dataGridViewSubjects.Name = "dataGridViewSubjects";
+            this.dataGridViewSubjects.RowTemplate.Height = 28;
+            this.dataGridViewSubjects.Size = new System.Drawing.Size(754, 333);
+            this.dataGridViewSubjects.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // specializationIdDataGridViewTextBoxColumn
+            // 
+            this.specializationIdDataGridViewTextBoxColumn.DataPropertyName = "SpecializationId";
+            this.specializationIdDataGridViewTextBoxColumn.DataSource = this.specializationBindingSource;
+            this.specializationIdDataGridViewTextBoxColumn.DisplayMember = "Name";
+            this.specializationIdDataGridViewTextBoxColumn.HeaderText = "SpecializationId";
+            this.specializationIdDataGridViewTextBoxColumn.Name = "specializationIdDataGridViewTextBoxColumn";
+            this.specializationIdDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.specializationIdDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.specializationIdDataGridViewTextBoxColumn.ValueMember = "Id";
+            // 
+            // specializationBindingSource
+            // 
+            this.specializationBindingSource.DataSource = typeof(WindowsFormsUniversity.Specialization);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // surnameDataGridViewTextBoxColumn
+            // 
+            this.surnameDataGridViewTextBoxColumn.DataPropertyName = "Surname";
+            this.surnameDataGridViewTextBoxColumn.HeaderText = "Surname";
+            this.surnameDataGridViewTextBoxColumn.Name = "surnameDataGridViewTextBoxColumn";
+            // 
+            // phoneDataGridViewTextBoxColumn
+            // 
+            this.phoneDataGridViewTextBoxColumn.DataPropertyName = "Phone";
+            this.phoneDataGridViewTextBoxColumn.HeaderText = "Phone";
+            this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataSource = typeof(WindowsFormsUniversity.Student);
+            // 
+            // idDataGridViewTextBoxColumn1
+            // 
+            this.idDataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn1.Name = "idDataGridViewTextBoxColumn1";
+            // 
+            // nameDataGridViewTextBoxColumn1
+            // 
+            this.nameDataGridViewTextBoxColumn1.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
+            // 
+            // amountOfHourDataGridViewTextBoxColumn
+            // 
+            this.amountOfHourDataGridViewTextBoxColumn.DataPropertyName = "AmountOfHour";
+            this.amountOfHourDataGridViewTextBoxColumn.HeaderText = "AmountOfHour";
+            this.amountOfHourDataGridViewTextBoxColumn.Name = "amountOfHourDataGridViewTextBoxColumn";
+            // 
+            // specializationIdDataGridViewTextBoxColumn1
+            // 
+            this.specializationIdDataGridViewTextBoxColumn1.DataPropertyName = "SpecializationId";
+            this.specializationIdDataGridViewTextBoxColumn1.DataSource = this.specializationBindingSource;
+            this.specializationIdDataGridViewTextBoxColumn1.DisplayMember = "Name";
+            this.specializationIdDataGridViewTextBoxColumn1.HeaderText = "SpecializationId";
+            this.specializationIdDataGridViewTextBoxColumn1.Name = "specializationIdDataGridViewTextBoxColumn1";
+            this.specializationIdDataGridViewTextBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.specializationIdDataGridViewTextBoxColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.specializationIdDataGridViewTextBoxColumn1.ValueMember = "Id";
+            // 
+            // subjectBindingSource
+            // 
+            this.subjectBindingSource.DataSource = typeof(WindowsFormsUniversity.Subject);
+            // 
+            // dataGridViewSpecializations
+            // 
+            this.dataGridViewSpecializations.AutoGenerateColumns = false;
+            this.dataGridViewSpecializations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSpecializations.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn2,
+            this.nameDataGridViewTextBoxColumn2});
+            this.dataGridViewSpecializations.DataSource = this.specializationBindingSource;
+            this.dataGridViewSpecializations.Location = new System.Drawing.Point(7, 7);
+            this.dataGridViewSpecializations.Name = "dataGridViewSpecializations";
+            this.dataGridViewSpecializations.RowTemplate.Height = 28;
+            this.dataGridViewSpecializations.Size = new System.Drawing.Size(754, 333);
+            this.dataGridViewSpecializations.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn2
+            // 
+            this.idDataGridViewTextBoxColumn2.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn2.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn2.Name = "idDataGridViewTextBoxColumn2";
+            // 
+            // nameDataGridViewTextBoxColumn2
+            // 
+            this.nameDataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn2.Name = "nameDataGridViewTextBoxColumn2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.tabControlStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.tabControlStudents.ResumeLayout(false);
+            this.tabPageStudents.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSubjects)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.specializationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSpecializations)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        private System.Windows.Forms.TabControl tabControlStudents;
+        private System.Windows.Forms.TabPage tabPageStudents;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private System.Windows.Forms.BindingSource specializationBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn specializationIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridViewSubjects;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountOfHourDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn specializationIdDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.BindingSource subjectBindingSource;
+        private System.Windows.Forms.DataGridView dataGridViewSpecializations;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn2;
+    }
+}
+
---
+++ 29.02.2020/WindowsFormsUniversity/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsUniversity
+{
+    public partial class Form1 : Form
+    {
+        UniversityEntities ctx = new UniversityEntities();
+        BindingSource bs1 = new BindingSource();
+        BindingSource bs2 = new BindingSource();
+        BindingSource bs3 = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+            bs1.DataSource = ctx.Students.Local.ToBindingList();
+            bs2.DataSource = ctx.Subjects.Local.ToBindingList();
+            bs3.DataSource = ctx.Specializations.Local.ToBindingList();
+
+            dataGridViewStudents.DataSource = bs1;
+            dataGridViewSubjects.DataSource = bs2;
+            dataGridViewSpecializations.DataSource = bs3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            studentBindingSource.DataSource = ctx.Students.ToList();
+            specializationBindingSource.DataSource = ctx.Specializations.ToList();
+            subjectBindingSource.DataSource = ctx.Subjects.ToList();
+        }
+
+        private void dataGridViewStudents_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            ctx.SaveChanges();
+
+            studentBindingSource.DataSource = ctx.Students.ToList();
+            specializationBindingSource.DataSource = ctx.Specializations.ToList();
+            subjectBindingSource.DataSource = ctx.Subjects.ToList();
+        }
+
+        private void dataGridViewStudents_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+           
+        }
+    }
+}
