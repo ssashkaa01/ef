7d2d8e981477efcf8abaad954aeff242bd77f5b0
STORYM <ssashkaa01@gmail.com>
2020-03-17 13:10:18 +0200

    add hw

---
+++ 27.03.2020/ClassLibrary/Tank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanksLib
+{
+    public class Tank
+    {
+        private string Name { get; set; }
+        private int Ammunition { get; set; }
+        private int Armor { get; set; }
+        private int Maneuverability { get; set; }
+        static Random rnd = new Random();
+
+        public Tank(string name)
+        {
+            var rand = new Random(name.GetHashCode());
+
+            Name = name;
+            Ammunition = rnd.Next(0, 100);
+            Armor = rnd.Next(0, 100);
+            Maneuverability = rnd.Next(0, 100);
+        }
+
+        public static bool operator ^(Tank t1, Tank t2)
+        {
+            int count = 0;
+
+            if (t1.Ammunition > t2.Ammunition) count++;
+            if (t1.Armor > t2.Armor) count++;
+            if (t1.Maneuverability > t2.Maneuverability) count++;
+
+            return count >= 2;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Ammunition: {Ammunition} Armor: {Armor} Maneuverability: {Maneuverability}";
+        }
+    }
+}
---
+++ 27.03.2020/WpfAppTanksDLL/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TanksLib;
+
+namespace WpfAppTanksDLL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread thread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tanks1.Items.Add(new Tank("T-34"));
+            Tanks1.Items.Add(new Tank("T-34"));
+            Tanks1.Items.Add(new Tank("T-34"));
+            Tanks1.Items.Add(new Tank("T-34"));
+            Tanks1.Items.Add(new Tank("T-34"));
+
+            Tanks2.Items.Add(new Tank("Panterra"));
+            Tanks2.Items.Add(new Tank("Panterra"));
+            Tanks2.Items.Add(new Tank("Panterra"));
+            Tanks2.Items.Add(new Tank("Panterra"));
+            Tanks2.Items.Add(new Tank("Panterra"));
+
+            thread = new Thread(War);
+            thread.Start();
+        }
+
+        public void War()
+        {
+            while(true)
+            {
+                Thread.Sleep(1000);
+
+                if(Tanks1.Items.Count == 0)
+                {
+                    MessageBox.Show("Tanks 2 WIN!!!");
+                    thread.Abort();
+                    break;
+                }
+                else if(Tanks2.Items.Count == 0)
+                {
+                    MessageBox.Show("Tanks 1 WIN!!!");
+                    thread.Abort();
+                    break;
+                }
+
+                Tank t1 = (Tank)Tanks1.Items[0];
+                Tank t2 = (Tank)Tanks2.Items[0];
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    if (t1 ^ t2)
+                    {
+                        Tanks2.Items.RemoveAt(0);
+                    }
+                    else
+                    {
+                        Tanks1.Items.RemoveAt(0);
+                    }
+                });
+            }
+        }
+    }
+}
