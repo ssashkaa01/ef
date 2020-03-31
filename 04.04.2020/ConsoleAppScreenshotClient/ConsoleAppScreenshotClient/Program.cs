e27dd4e9c9f93460a366e37fabc5f5282df5d653
STORYM <ssashkaa01@gmail.com>
2020-03-31 15:06:05 +0300

    fixed bugs

---
+++ 04.04.2020/ConsoleAppScreenshotClient/ConsoleAppScreenshotClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleAppScreenshotClient
+{
+    class Program
+    {
+        private static int IntervalSend { get; set; }
+        private static string ServerIp { get; set; }
+        private static int ServerPort { get; set; }
+
+        static void Main()
+        {
+            ServerIp = "127.0.0.1";
+            ServerPort = 12345;
+            IntervalSend = 5000;
+
+            StartSender();
+        }
+
+        private static void StartSender()
+        {
+
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ServerIp), ServerPort);
+
+                bool done = false;
+                while (!done)
+                {
+                    Thread.Sleep(IntervalSend);
+
+                    Bitmap bmp = new Bitmap(200, 200);
+                    //Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                    }
+
+                    ImageConverter converter = new ImageConverter();
+                    byte []image = (byte[])converter.ConvertTo(bmp, typeof(byte[]));
+
+                    Console.WriteLine("Take screenshot!");
+
+                    try
+                    {
+                        s.SendTo(image, ep);
+                        Console.WriteLine("Screenshot sent!");
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    
+
+                    
+
+                }
+            }
+        }
+    }
+}
---
+++ 04.04.2020/WpfAppScreenshot/WpfAppScreenshot/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppScreenshot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool IsStarted { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IsStarted = false;
+            btnStop.IsEnabled = false;
+        }
+
+        private async void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int PORT;
+
+            try
+            {
+                PORT = Convert.ToInt32(textPORT.Text);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            IsStarted = true;
+            textPORT.IsEnabled = false;
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+
+            await Task.Run(() =>
+            {
+                StartListener(PORT);
+            });
+           
+        }
+
+        private void StartListener(int port)
+        {
+           
+            UdpClient listener = new UdpClient(port);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
+
+            try
+            {
+                while (IsStarted)
+                {
+                    byte[] bytes = listener.Receive(ref groupEP);
+                  
+                    MemoryStream byteStream = new MemoryStream(bytes);
+
+                    this.Dispatcher.Invoke(() =>
+                    {
+
+                        BitmapImage image = new BitmapImage();
+                        image.BeginInit();
+                        image.StreamSource = byteStream;
+                        image.EndInit();
+                        
+                        imageBox.Source = image;
+                    });
+                   
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            textPORT.IsEnabled = true;
+            IsStarted = false;
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+        }
+    }
+}
